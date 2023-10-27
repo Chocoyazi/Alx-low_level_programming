de47f5c6c51b95a3c4d4560fb3cd9bf0d6e2cf01
chocoyazi <chocoyazi@github.com>
2023-10-26 18:40:55 -0700

    new commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H
-#define _MAIN_H
 int is_palindrome(char *s);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
@@ -9,4 +7,3 @@ int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-#endif
